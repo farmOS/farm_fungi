54cb42859f27f8d2b086d4d24fa22e874bd6e158
Michael Stenta <mike@mstenta.net>
2021-01-05 09:03:56 -0500

    Add Fungi asset type.

---
+++ src/Plugin/Asset/AssetType/Fungi.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_fungi\Plugin\Asset\AssetType;
+
+use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+
+/**
+ * Provides the fungi asset type.
+ *
+ * @AssetType(
+ *   id = "fungi",
+ *   label = @Translation("Fungi"),
+ * )
+ */
+class Fungi extends FarmAssetType {
+
+}
