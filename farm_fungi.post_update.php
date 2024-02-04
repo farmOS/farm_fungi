805276b5e526f079f5789d7496c77857310eafe1
Michael Stenta <mike@mstenta.net>
2024-02-04 08:09:53 -0500

    Fix Error: Class "Drupal\migrate_plus\Entity\Migration" not found
    This happens if the migrate_plus module is not installed when
    the update hook runs.

--- farm_fungi.post_update.php
+++ farm_fungi.post_update.php
@@ -5,21 +5,19 @@
  * Post update hooks for the farm_fungi module.
  */
 
-use Drupal\migrate_plus\Entity\Migration;
-
 /**
  * Uninstall v1 migration configs.
  */
 function farm_fungi_post_update_uninstall_v1_migrations(&$sandbox) {
-  $config = Migration::load('farm_migrate_asset_fungi');
+  $config = \Drupal::configFactory()->getEditable('migrate_plus.migration.farm_migrate_asset_fungi');
   if (!empty($config)) {
     $config->delete();
   }
-  $config = Migration::load('farm_migrate_taxonomy_fungi_type');
+  $config = \Drupal::configFactory()->getEditable('migrate_plus.migration.farm_migrate_taxonomy_fungi_type');
   if (!empty($config)) {
     $config->delete();
   }
-  $config = Migration::load('farm_migrate_taxonomy_substrate_type');
+  $config = \Drupal::configFactory()->getEditable('migrate_plus.migration.farm_migrate_taxonomy_substrate_type');
   if (!empty($config)) {
     $config->delete();
   }
