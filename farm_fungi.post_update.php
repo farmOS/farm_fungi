6beeeb8c5d0166c41a0e29816d57728016406cd6
Michael Stenta <mike@mstenta.net>
2026-02-13 16:56:18 -0500

    Add missing hook_removed_post_updates() docblock.

--- farm_fungi.post_update.php
+++ farm_fungi.post_update.php
@@ -5,6 +5,9 @@
  * Post update hooks for the farm_fungi module.
  */
 
+/**
+ * Implements hook_removed_post_updates().
+ */
 function farm_fungi_removed_post_updates() {
   return [
     'farm_fungi_post_update_uninstall_v1_migrations' => '3.x',
